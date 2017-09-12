c910057364dfcd943212367f47911a93533a2351
sanjeeviaa <30828471+sanjeeviaa@users.noreply.github.com>
2017-09-12 15:54:01 +0530

    Create sanjeevi.c

---
+++ sanjeevi.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int ;
+scanf<"%d",&n);
+if(n>0)
+{
+printf("the number is positive");
+}
+else if(n<0)
+{
+printf("the number is negative");
+}
+else
+{
+printf(" the number is zero");
+}
+}
